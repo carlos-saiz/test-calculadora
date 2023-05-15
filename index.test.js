7c706a4f3dac502174c918427dba0adc2790b5b2
carlos-saiz <useles123456789@gmail.com>
2023-05-15 12:47:21 +0200

    hemos intentado los test

--- index.test.js
+++ index.test.js
@@ -1,1 +1,20 @@
-import test from 'node:test';g
+import test from 'node:test';
+const assert = require('assert');
+const sum = require('sum.js');
+
+// Prueba 1: Suma de dos números positivos
+assert.strictEqual(sum(2, 3), 5);
+
+// Prueba 2: Suma de un número positivo y uno negativo
+assert.strictEqual(sum(-5, 10), 5);
+
+// Prueba 3: Suma de dos números negativos
+assert.strictEqual(sum(-2, -3), -5);
+
+// Prueba 4: Suma de cero y un número positivo
+assert.strictEqual(sum(0, 5), 5);
+
+// Prueba 5: Suma de cero y un número negativo
+assert.strictEqual(sum(0, -5), -5);
+
+console.log('Todas las pruebas han pasado correctamente.');
